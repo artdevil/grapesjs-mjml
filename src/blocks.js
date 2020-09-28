58f5fea6dae8f00fbabd8d0e94542f7e5eb9a2a2
adhiguna <adhiguna.sabril@gmail.com>
2020-09-29 06:22:30 +0700

    image close tag

--- src/blocks.js
+++ src/blocks.js
@@ -52,7 +52,7 @@ export default (editor, opt = {}) => {
 
   bm.add('mj-image', {
     label: editor.I18n.t('grapesjs-mjml.components.names.image'),
-    content: '<mj-image src="http://placehold.it/350x250/78c5d6/fff"/>',
+    content: '<mj-image src="http://placehold.it/350x250/78c5d6/fff"></mj-image>',
     attributes: { class: 'fa fa-image' },
     ...allBlocks,
   });
@@ -127,7 +127,7 @@ export default (editor, opt = {}) => {
     content: `<mj-wrapper border="1px solid #000000" padding="50px 30px">
     <mj-section border-top="1px solid #aaaaaa" border-left="1px solid #aaaaaa" border-right="1px solid #aaaaaa" padding="20px">
       <mj-column>
-        <mj-image padding="0" src="http://placehold.it/350x250/78c5d6/fff" />
+        <mj-image padding="0" src="http://placehold.it/350x250/78c5d6/fff"></mj-image>
       </mj-column>
     </mj-section>
     <mj-section border-left="1px solid #aaaaaa" border-right="1px solid #aaaaaa" padding="20px" border-bottom="1px solid #aaaaaa">
